602b02fdab5a9f048e3168df45f33d9877a33277
Luiz Andre <luizxd234@gmail.com>
2020-07-08 21:03:55 -0300

    feat: Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
+import Routes from './routes';
+
 const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <h1>Hello World</h1>
-    </>
+      <Routes />
+    </Router>
   );
 };
 
